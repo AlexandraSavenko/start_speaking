e4a532ce5b91e14f5d13bef0c2d6bb76ef145400
Sasha Savenko <sashasavenko3@gmail.com>
2026-08-25 21:30:03 +0300

    intro fix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,12 +2,10 @@ import css from "./Intro.module.css";
 
 const Intro = () => {
   return (
-    <div>
       <div className={css.hero}>
           <h1 className={css.title}>Welcome to Start Speaking Course</h1>
           <img src="/hero.png" alt="hero"/>
         </div>
-    </div>
   );
 };
 
