6cb4512e49e522b6c6c48dd0056339bcf06a1da7
Sasha Savenko <sashasavenko3@gmail.com>
2026-08-31 11:05:49 +0300

    Speaking task fix

--- src/components/imageBox/ImageBox.jsx
+++ src/components/imageBox/ImageBox.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import SpeakButton from "../speakButton/SpeakButton";
 import css from "./ImageBox.module.css";
 
-const ImageBox = ({ images }) => {
+const ImageBox = ({ images}) => {
   const [isGallery, setIsGallery] = useState(false);
   console.log("images:", images);
   console.log("first image:", images[0]);
@@ -18,7 +18,7 @@ const ImageBox = ({ images }) => {
         <div key={el.url} className={css.card}>
           <img className={css.image} src={`/task_images/lesson1/${el.url}.png`} alt={el.label} />
           {el.label && <div className={css.textBox}>
-            <SpeakButton text={el.label} />
+            <SpeakButton text={el.answer} />
             <p>{el.label}</p>
           </div>}
           
