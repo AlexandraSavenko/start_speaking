93b755d9a2290f52c50c85b9716c80b631e296eb
Sasha Savenko <sashasavenko3@gmail.com>
2026-09-25 18:50:29 +0300

    lesson 6

--- src/lessons/index.js
+++ src/lessons/index.js
@@ -3,6 +3,7 @@ import { lesson2 } from "./lesson2/data";
 import { lesson3 } from "./lesson3/data";
 import { lesson4 } from "./lesson4/data";
 import { lesson5 } from "./lesson5/data";
+import { lesson6 } from "./lesson6/data";
 
 
-export const lessons = [lesson1, lesson2, lesson3, lesson4, lesson5];
+export const lessons = [lesson1, lesson2, lesson3, lesson4, lesson5, lesson6];
---
+++ src/lessons/lesson6/data.js
@@ -0,0 +1,191 @@
+export const lesson6 = {
+  id: "lesson6",
+  title: "In my backpack.",
+  video: ["dbklZrO5H78"],
+  prelistening: [
+    {
+      id: "task1",
+      title: "Song_Nouns",
+      type: "Look and say",
+      images: [
+        { url: "bluecrayon", label: "a crayon" },
+        { url: "yellowpencil", label: "a pencil" },
+        { url: "redmarker", label: "a marker" },
+        { url: "greenruler", label: "a ruler" },
+        { url: "redgluestick", label: "a glue stick" },
+        { url: "lunchbox", label: "a lunchbox" },
+        { url: "bluebackpack", label: "a backpack" },
+        { url: "whitepaper", label: "a paper" },
+      ],
+    },
+    {
+      id: "task2",
+      title: "Nouns",
+      type: "Look and say",
+      images: [
+        { url: "stick", label: "a stick" },
+        { url: "back", label: "a back" },
+         { url: "box", label: "a box" },
+      ],
+    },
+  ],
+  pages: [
+    {
+      id: 1,
+      altText: "Animal circus",
+      text: "Hello, I'm Peter, the pencil. I am blue.",
+    },
+  ],
+  vocabulary: {
+    nouns: [
+      { word: "crayon", meaning: "кольоровий мелок або олівець" },
+      { word: "pencil", meaning: "олівець" },
+      { word: "marker", meaning: "маркер" },
+      { word: "ruler", meaning: "лінійка" },
+      { word: "glue stick", meaning: "клей олівець" },
+      { word: "lunchbox", meaning: "коробка для шкільного обіду" },
+      { word: "backpack", meaning: "рюкзак" },
+      { word: "paper", meaning: "папір" },
+      { word: "stick", meaning: "палка" },
+      { word: "back", meaning: "спина" },
+      { word: "box", meaning: "коробка" },
+    ],
+    phrases: [
+      { word: "Do you have ...", meaning: "В тебе є ..." },
+      { word: "Yes, I do.", meaning: "Так, є." },
+      { word: "No, I do not.", meaning: "Ні, немає." },
+    ],
+  },
+  speaking: [
+    {
+      id: "task1",
+      title: "It is in the backpack.",
+      type: "Look and say",
+      dialogue: [{ speaker: "A", line: "Where is ruler?" }, { speaker: "B", line: "It is in the backpack." }],
+      table: [
+        {
+            column1: "It",
+          column2: "is",
+          column3: ["on", "in", "under", "by"],
+          column4: "the backpack.", 
+        },
+      ],
+      images: [
+        { url: "bagruler", label: "in the backpack", question: "Where is ruler?", answer: "It is in the backpack.",  },
+        { url: "bagpencil", label: "on the backpack", question: "Where is pencil?", answer: "It is on the backpack.", },
+        { url: "baglunchbox", label: "under the backpack", question: "Where is lunchbox?", answer: "It is by the backpack.", },
+        { url: "bagglue", label: "by the backpack", question: "Where is glue stick?", answer: "It is by the backpack.", },
+        { url: "bagpaper", label: "under the backpack", question: "Where is paper?", answer: "It is under the backpack.", },
+        { url: "bagcrayon", label: "on the backpack", question: "Where is crayon?", answer: "It is on the backpack.", },
+        { url: "bagmarker", label: "by the backpack", question: "Where is marker?", answer: "It is by the backpack.", },
+        
+      ],
+    },
+    {
+      id: "task2",
+      title: "I see two green crayons.",
+      type: "Look and say",
+      dialogue: [{ speaker: "A", line: "How many green crayons?" }, { speaker: "B", line: "I see two green crayons." }],
+      table: [
+        {
+            column1: "I",
+          column2: "see",
+          column3: ["one", "two", "three", "four", "five", "six"],
+          column4: ["green", "blue", "red", "yellow", "brown"],
+          column5: ["crayon", "marker", "glue stick", "ruler", "pencil"],
+           column6: "s",
+        },
+      ],
+      images: [
+        { url: "greencrayons", label: "crayons", question: "How many green crayons?", answer: "I see two green crayons.",  },
+        { url: "bluemarkers", label: "markers", question: "How many blue markers?", answer: "I see two blue markers.", },
+        { url: "redglue", label: "glue sticks", question: "How many red glue sticks?", answer: "I see one red glue stick.", },
+        { url: "yellowrulers", label: "rulers", question: "How many yellow rulers?", answer: "I see three yellow rulers.", },
+        { url: "brownpencils", label: "pencils", question: "How many brown pencils?", answer: "I see one brown pencil.", },
+      ],
+    },
+    {
+      id: "task3",
+      title: "It is a blue crayon.",
+      type: "Look and say",
+      dialogue: [{ speaker: "A", line: "What is it?" }, { speaker: "B", line: "It is a blue crayon" }],
+      table: [
+        {
+            column1: "It",
+          column2: "is",
+          column3: "a",
+          column4: ["white",
+            "grey",
+            "blue",
+            "red",
+            "yellow",
+            ],
+          column5: ["crayon", "pencil", "ruler", "marker", "paper", "glue stick", "lunchbox", "backpack" ]
+        },
+      ],
+      images: [
+        { url: "bluecrayon", label: "a blue crayon", question: "What is it?", answer: "It is a blue crayon", }, 
+        { url: "yellowpencil", label: "an yellow pencil", question: "What is it?", answer: "It is a yellow pencil.", },
+        { url: "greenruler", label: "a green ruler", question: "What is it?", answer: "It is a green ruler.", },
+       { url: "redmarker", label: "a red marker", question: "What is it?", answer: "It is a red marker.", },
+        { url: "whitepaper", label: "white paper", question: "What is it?", answer: "It is white paper.", },
+        { url: "redgluestick", label: "a red glue stick", question: "What is it?", answer: "It is a red glue stick.", },
+        { url: "redlunchbox", label: "a red lunch box", question: "What is it?", answer: "It is a red lunchbox.", },
+        { url: "bluebackpack", label: "a blue backpack", question: "What is it?", answer: "It is a blue backpack.", },
+      ],
+    },
+    {
+      id: "task4",
+      title: "A camel has the pencil.",
+      type: "Look and say",
+      dialogue: [{ speaker: "A", line: "Who has a pencil?" }, { speaker: "B", line: "A camel has a pencil." }],
+      table: [
+        {
+            column1: ["A camel", "a mouse", "a fish", "a bird", "a lion", "a lizard"],
+          column2: "has",
+          column3: ["a crayon", "a pencil", "a ruler", "a marker", "a paper", "a glue stick", "a lunchbox", "a backpack" ]
+        },
+      ],
+      images: [
+        { url: "camelpencil", label: "a camel and a pencil", question: "Who has a pencil?", answer: "A camel has a pencil", }, 
+        { url: "mouselunchbox", label: "a mouse and a lunchbox", question: "Who has a lunchbox?", answer: "A mouse has a lunchbox", },
+        { url: "fishruler", label: "a fish and a ruler", question: "Who has a ruler?", answer: "A fish has a ruler", },
+        { url: "birdgluestick", label: "a bird and a glue", question: "Who has a glue stick?", answer: "A bird has a glue stick", },
+        { url: "lioncrayons", label: "a lion and crayons", question: "Who has crayons?", answer: "A lion has crayons", },
+        { url: "lizardbackpack", label: "a lizard and a backpack", question: "Who has a backpack?", answer: "A lizard has a backpack", },
+
+      ],
+    },
+    {
+      id: "task5",
+      title: "One ruler and three pencils.",
+      type: "Look and say",
+      dialogue: [{ speaker: "A", line: "How many?" }, { speaker: "B", line: "One ruler and three pencils." }],
+      table: [
+        {
+            column1: ["one", "two", "three", "four", "five", "six"],
+          column2: ["a crayon", "a pencil", "a ruler", "a marker", "a paper", "a glue stick", "a lunchbox", "a backpack" ],
+          column3: "and",
+          column4: ["one", "two", "three", "four", "five", "six"],
+          column5: ["a crayon", "a pencil", "a ruler", "a marker", "a paper", "a glue stick", "a lunchbox", "a backpack" ]
+        },
+      ],
+      images: [
+        { url: "rulerpencils", label: "ruler and pencils", question: "How many?", answer: "one ruler and three pencils", }, 
+        { url: "markersgluestick", label: "markers and a gluestick", question: "How many?", answer: "five markers and a gluestick", },
+        { url: "crayonsmarkers", label: "crayons and markers", question: "How many?", answer: "six crayons and two markers", },
+        { url: "lunchboxbackpack", label: "a lunchbox and a backpack", question: "How many?", answer: "one lunchbox and one backpack", },
+        { url: "backpackspaper", label: "backpacks and a paper", question: "How many?", answer: "four backpacks and a paper", },
+        { url: "rulerscrayons", label: "rulers and crayons", question: "How many?", answer: "three rulers and two crayons", },
+
+      ],
+    },
+
+  ],
+  games: [
+    {
+      url: "https://learningapps.org/watch?v=p3oo8b1va26",
+    },
+    
+  ],
+};
